3bae8a7585d47da7a3665724216c9808cf3f75f3
Gisele Cesar <gisele-cesar@hotmail.com>
2020-10-06 14:30:36 -0300

    Inclusão de arquivo no git

---
+++ 05-ByteBank/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Cliente gabriela = new Cliente();
+
+            // gabriela.nome = "Gabriela";
+            // gabriela.profissao = "Desenvolvedora C#";
+            // gabriela.cpf = "123.456.789-10";
+
+            ContaCorrente conta = new ContaCorrente();
+            conta.titular = new Cliente();
+
+            conta.titular.nome = "Gisele";
+
+            // conta.titular = gabriela;
+            // conta.titular = new Cliente();
+            // conta.titular.nome = "Gabriela Costa";
+            // conta.titular.cpf = "123.456.789-10";
+            // conta.titular.profissao = "Desenvolvedora C#";
+
+            conta.saldo = 500;
+            conta.agencia = 563;
+            conta.numero = 563527;
+
+            if(conta.titular == null)
+            {
+                Console.WriteLine("Ops, areferência em conta.titular é NULL");
+            }
+
+            // Console.WriteLine(gabriela.nome);
+            Console.WriteLine(conta.titular.nome);
+            // Console.WriteLine(conta.titular.cpf);
+            // Console.WriteLine(conta.titular.profissao);
+
+            Console.ReadLine();
+
+        }
+    }
+}
